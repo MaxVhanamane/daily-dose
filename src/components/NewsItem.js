4f467c373ff0dff3a09e22127111eba22bfa1ec1
Max <maxvhanamane@gmail.com>
2022-09-20 19:58:15 +0530

    Changed card styling

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,8 +5,8 @@ const [date]=useState(new Date(props.time).toGMTString().toString())
         return (
 
 <>
-            <div className="card m-2 " >
-                <img width="474" height="266" style={{objectFit:"cover"}} src={props.imageUrl ? props.imageUrl : "https://media4.s-nbcnews.com/i/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.png"} className="card-img-top" alt="..." />
+            <div className="card m-2 shadow " >
+                <img width="474" height="266"  src={props.imageUrl ? props.imageUrl : "https://media4.s-nbcnews.com/i/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.png"} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title"> {props.title}</h5>
                     <p className="card-text ">{props.description.substring(0, 100) + "..."}</p>
